3b6bbf92b62a05b0b911c85349371fa93af8f70d
Darshil Modi <45987056+darshil3011@users.noreply.github.com>
2022-11-30 11:38:54 +0530

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -173,21 +173,23 @@ if user_input:
         train_input = st.session_state.train
         coach_line, departure_station, arrival_station, station_list = train_info(int(train_input))
         location = get_loc(station_list)
-        for i in station_list:
-            if location in str(i).lower():
-                output = i
+        #for i in station_list:
+        #    if location in str(i).lower():
+        #        output = i
         #station_table = pd.DataFrame.from_dict(station_list, orient='columns')
-        d = output
-        if len(d['arrival']) != 0 and len(d['platform']) != 0 and len(d['hault']) != 0: 
-            output_string = 'At ' + d['name'][0] + ' train will arrive on platform number ' + str(d['platform'][0]) + ' at ' + str(d['arrival'][0]) + ' and will hault for ' + str(d['hault'][0])
-        elif len(d['arrival']) == 0:
-            output_string = 'At ' + d['name'][0] + ' train will start from platform number ' + str(d['platform'][0]) + ' and will depart at ' + str(d['departure'][0]) 
-        elif len(d['platform']) == 0:
-            output_string = 'At ' + d['name'][0] + ' train will arrive at ' + str(d['arrival'][0]) + ' and will hault for ' + str(d['hault'][0]) + '. Platform is not yet decided !' 
-        elif len(d['departure']) == 0:
-            output_string = 'At ' + d['name'][0] + ' train will arrive on platform number ' + str(d['platform'][0]) + ' at ' + str(d['arrival'][0]) + '. This is the final destination.'
-        response = str(output_string)
-
+        d = location
+        
+        #if len(d['arrival']) != 0 and len(d['platform']) != 0 and len(d['hault']) != 0: 
+        #    output_string = 'At ' + d['name'][0] + ' train will arrive on platform number ' + str(d['platform'][0]) + ' at ' + str(d['arrival'][0]) + ' and will hault for ' + str(d['hault'][0])
+        #elif len(d['arrival']) == 0:
+        #    output_string = 'At ' + d['name'][0] + ' train will start from platform number ' + str(d['platform'][0]) + ' and will depart at ' + str(d['departure'][0]) 
+        #elif len(d['platform']) == 0:
+        #    output_string = 'At ' + d['name'][0] + ' train will arrive at ' + str(d['arrival'][0]) + ' and will hault for ' + str(d['hault'][0]) + '. Platform is not yet decided !' 
+        #elif len(d['departure']) == 0:
+        #    output_string = 'At ' + d['name'][0] + ' train will arrive on platform number ' + str(d['platform'][0]) + ' at ' + str(d['arrival'][0]) + '. This is the final destination.'
+        
+        #response = str(output_string)
+        response = "your station is: " + d
     elif intent == 5:
         response = "hmmm.. what else ?" 
     
